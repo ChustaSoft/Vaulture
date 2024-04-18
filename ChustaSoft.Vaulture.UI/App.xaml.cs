8a22502d0043b5c245854f5e9df5460a62716cd2
Xelit3 <xavi_abelaira@outlook.com>
2024-04-18 23:26:38 +0200

    WPF-UI integrated, basic layout done

--- ChustaSoft.Vaulture.UI/App.xaml.cs
+++ ChustaSoft.Vaulture.UI/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace Vaulture
+namespace ChustaSoft.Vaulture.UI
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ ChustaSoft.Vaulture.UI/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ChustaSoft.Vaulture.UI.Pages
+{
+    /// <summary>
+    /// Interaction logic for DashboardPage.xaml
+    /// </summary>
+    public partial class DashboardPage : Page
+    {
+        public DashboardPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
