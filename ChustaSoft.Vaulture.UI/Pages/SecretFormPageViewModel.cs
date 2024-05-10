5cccda4ec0e5780f77328c7fb9c890bc455475bc
Xelit3 <xavi_abelaira@outlook.com>
2024-05-11 01:02:59 +0200

    Initial SecretFormPage definition

--- ChustaSoft.Vaulture.UI/Pages/SecretFormPageViewModel.cs
+++ ChustaSoft.Vaulture.UI/Pages/SecretFormPageViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace ChustaSoft.Vaulture.UI.Pages
+﻿namespace ChustaSoft.Vaulture.UI.Pages;
+
+
+public partial class SecretFormPageViewModel : ObservableObject
 {
-    public partial class SecretFormPageViewModel : ObservableObject
+
+    [ObservableProperty]
+    private bool enableSaveAction = false;
+
+
+    [RelayCommand]
+    private void OnSave()
     {
+        //TODO: Save new secret
+
+        EnableSaveAction = false;
     }
 }
