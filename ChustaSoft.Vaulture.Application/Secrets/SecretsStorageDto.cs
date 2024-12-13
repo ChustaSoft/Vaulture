a550ae806192f4c5f96b565b58c95a4534278195
Xelit3 <xavi_abelaira@outlook.com>
2024-12-13 17:06:19 +0100

    application layer refactored

---
+++ ChustaSoft.Vaulture.Application/Secrets/SecretsStorageDto.cs
@@ -0,0 +1,8 @@
+﻿using ChustaSoft.Vaulture.Domain.Secrets;
+
+namespace ChustaSoft.Vaulture.Application.Secrets;
+
+public record struct SecretsStorageDto(SecretsStorageType Type, string Alias, string Value)
+{
+    public override string ToString() => $"{Alias} ({Value})";
+};
---
+++ ChustaSoft.Vaulture.Application/Secrets/SecretsStorageTypeDto.cs
@@ -0,0 +1,8 @@
+﻿using ChustaSoft.Vaulture.Domain.Secrets;
+
+namespace ChustaSoft.Vaulture.Application.Secrets;
+
+public record struct SecretsStorageTypeDto(SecretsStorageType Type, string Name)
+{
+    public override string ToString() => Name;
+};
